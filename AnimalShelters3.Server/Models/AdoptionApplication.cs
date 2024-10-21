4d2861b04a61034a33f517a7bdb9bb44f2260b2c
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-10-21 13:25:49 +0300

    ignore app setting

---
+++ AnimalShelters3.Server/Models/AdoptionApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalShelters3.Server.Models;
+
+public partial class AdoptionApplication
+{
+    public int ApplicationId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? AnimalId { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? SubmittedAt { get; set; }
+
+    public string? Message { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual Animal? Animal { get; set; }
+
+    public virtual User? User { get; set; }
+}
---
+++ AnimalShelters3.Server/Models/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalShelters3.Server.Models;
+
+public partial class Animal
+{
+    public int AnimalId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public long? CategoryId { get; set; }
+
+    public string? Breed { get; set; }
+
+    public int Age { get; set; }
+
+    public int? ShelterId { get; set; }
+
+    public string? Temperament { get; set; }
+
+    public string? AdoptionStatus { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<AdoptionApplication> AdoptionApplications { get; set; } = new List<AdoptionApplication>();
+
+    public virtual Category? Category { get; set; }
+
+    public virtual Shelter? Shelter { get; set; }
+}
