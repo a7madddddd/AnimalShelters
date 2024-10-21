0df32a738cc14cd8705e0f2b25e491d2055ba9dd
najlaaalsmadi <najlaak399@gmail.com>
2024-10-21 17:14:28 +0300

    add controllers+update database

---
+++ AnimalShelters3.Server/Controllers/CommunityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalShelters3.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunityController : ControllerBase
+    {
+
+    }
+}
