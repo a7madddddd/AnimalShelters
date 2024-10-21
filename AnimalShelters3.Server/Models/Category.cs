6b8c3fa9166d61757335432835df9be61a02be42
ahmadqadomi-1 <qadomia95@gmail.com>
2024-10-21 13:30:16 +0300

    Add

---
+++ AnimalShelters3.Server/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalShelters3.Server.Models;
+
+public partial class Category
+{
+    public long Id { get; set; }
+
+    public string Species { get; set; } = null!;
+
+    public string? Image { get; set; }
+
+    public virtual ICollection<Animal> Animals { get; set; } = new List<Animal>();
+}
