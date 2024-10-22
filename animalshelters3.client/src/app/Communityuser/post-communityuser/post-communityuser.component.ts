edfd5ec9db1445d0f8620823ef365977b4fb568d
najlaaalsmadi <najlaak399@gmail.com>
2024-10-22 15:08:09 +0300

    like and sharePost

--- animalshelters3.client/src/app/Communityuser/post-communityuser/post-communityuser.component.ts
+++ animalshelters3.client/src/app/Communityuser/post-communityuser/post-communityuser.component.ts
@@ -1,49 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NajlaaService } from '../../../Services/najlaa.service';
+import { BehaviorSubjectService } from '../../newProject9/Lujain/BehaviorSubject/behavior-subject.service';
+import { HttpClient } from '@angular/common/http'; // Import HttpClient for API calls
+
+declare var bootstrap: any; // To use Bootstrap's JS functionality
 
 @Component({
   selector: 'app-post-communityuser',
   templateUrl: './post-communityuser.component.html',
-  styleUrl: './post-communityuser.component.css'
+  styleUrls: ['./post-communityuser.component.css']
 })
-export class PostCommunityuserComponent {
-
+export class PostCommunityuserComponent implements OnInit {
 
   approvedPosts: any[] = [];
+  currentUserId: string | undefined;
+  selectedPost: any; // Holds the post selected for sharing
 
-  constructor(private najlaaService: NajlaaService) { }
-  //toggleLike(post: any) {
-  //  const currentUserId = this.userId.value; // الحصول على userId الفعلي
-
-  //  if (!currentUserId) {
-  //    console.error('User not logged in');
-  //    return; // عدم متابعة إذا لم يكن هناك مستخدم مسجل الدخول
-  //  }
-
-  //  this.najlaaService.likePost(post.id, currentUserId).subscribe(
-  //    (response: any) => {
-  //      console.log(response.message);
-
-  //      if (response.message === 'Post liked') {
-  //        post.likesCount++;
-  //      } else if (response.message === 'Like removed') {
-  //        post.likesCount--;
-  //      }
-  //    },
-  //    (error: any) => {
-  //      console.error('Error liking post', error);
-  //    }
-  //  );
-  //}
+  constructor(private najlaaService: NajlaaService, private behaviorSubjectService: BehaviorSubjectService, private http: HttpClient) { }
 
   ngOnInit() {
+    // Subscribe to userId$ to get the current user ID
+    this.behaviorSubjectService.userId$.subscribe(userId => {
+      this.currentUserId = userId;
+    });
+
+    // Fetch all approved posts
     this.najlaaService.getAllApprovedPosts().subscribe(
       (data) => {
         this.approvedPosts = data;
-        console.log(this.approvedPosts); 
+        console.log(this.approvedPosts);
       },
       (error) => {
         console.error('Error fetching approved posts', error);
+      }
+    );
+  }
+
+  // Toggle like functionality
+  toggleLike(post: any) {
+    if (!this.currentUserId) {
+      console.error('User not logged in');
+      return;
+    }
+
+    this.najlaaService.likePost(post.id, Number(this.currentUserId)).subscribe(
+      (response: any) => {
+        console.log(response.message);
+
+        if (response.message === 'Post liked') {
+          post.likesCount++;
+        } else if (response.message === 'Like removed') {
+          post.likesCount--;
+        }
+      },
+      (error: any) => {
+        console.error('Error liking post', error);
+      }
+    );
+  }
+
+  // Open the modal for sharing
+  openShareModal(post: any) {
+    this.selectedPost = post;
+    const shareModal = new bootstrap.Modal(document.getElementById('shareModal') as HTMLElement);
+    shareModal.show();
+  }
+
+  // Share on WhatsApp
+  shareOnWhatsApp(post: any) {
+    this.http.get(`https://localhost:44354/api/Community/sharePost/whatsapp/${post.id}`).subscribe(
+      (response: any) => {
+        window.open(response.shareUrl, '_blank');
+      },
+      (error) => {
+        console.error('Error sharing post on WhatsApp', error);
+      }
+    );
+  }
+
+  // Share on Facebook
+  shareOnFacebook(post: any) {
+    this.http.get(`https://localhost:44354/api/Community/sharePost/facebook/${post.id}`).subscribe(
+      (response: any) => {
+        window.open(response.shareUrl, '_blank');
+      },
+      (error) => {
+        console.error('Error sharing post on Facebook', error);
       }
     );
   }
