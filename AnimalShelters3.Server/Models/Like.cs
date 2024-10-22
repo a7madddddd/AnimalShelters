9a1647f3fcd63f7f67c6ef8ed3757e5981c383d8
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-10-22 09:57:37 +0300

    add najla`a edits

--- AnimalShelters3.Server/Models/Like.cs
+++ AnimalShelters3.Server/Models/Like.cs
@@ -11,6 +11,10 @@ public partial class Like
 
     public int? UserId { get; set; }
 
+    public long? CommentId { get; set; }
+
+    public virtual Comment? Comment { get; set; }
+
     public virtual Post? Post { get; set; }
 
     public virtual User? User { get; set; }
