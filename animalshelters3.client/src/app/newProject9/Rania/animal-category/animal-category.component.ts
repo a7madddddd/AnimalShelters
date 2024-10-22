9c85c7fd8b01cf86cef46e65ca711c181df12457
raniahasi <raniahasi@gmail.com>
2024-10-22 15:00:46 +0300

    animals

--- animalshelters3.client/src/app/newProject9/Rania/animal-category/animal-category.component.ts
+++ animalshelters3.client/src/app/newProject9/Rania/animal-category/animal-category.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';  // Import ViewEncapsulation
 import { AnimalService } from '../services/animal-service.service';
-import { Animal } from '../../../../shared/interfaces';  // adjust the relative path
+import { Animal } from '../../../../shared/interfaces';  // Adjust the relative path
 
 @Component({
   selector: 'app-animal-category',
   templateUrl: './animal-category.component.html',
+  styleUrls: ['./animal-category.component.css'],  // Include your styles file
+  encapsulation: ViewEncapsulation.None  // Disable view encapsulation
 })
 export class AnimalCategoryComponent implements OnInit {
   animals: Animal[] = [];  // Define animals array
