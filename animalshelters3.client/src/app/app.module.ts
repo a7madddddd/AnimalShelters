d797bdd18cddb6ff8b7786535d0febca7f631b06
lujain998 <lujainalazzam98@gmail.com>
2024-10-22 12:48:19 +0300

    Login

--- animalshelters3.client/src/app/app.module.ts
+++ animalshelters3.client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AnimalsComponent } from './newProject9/A7mad/animals/animals.component'
 import { AboutUSComponent } from './newProject9/Qadomi/about-us/about-us.component';
 import { PostCommunityuserComponent } from './Communityuser/post-communityuser/post-communityuser.component';
 import { AnimalCategoryComponent } from './newProject9/Rania/animal-category/animal-category.component';
+import { LoginComponent } from './newProject9/Lujain/login/LoginComponent';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { AnimalCategoryComponent } from './newProject9/Rania/animal-category/ani
     PostCommunityuserComponent,
     HomeComponent,
     AnimalCategoryComponent,
-    AboutUSComponent
+    AboutUSComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
@@ -36,12 +38,14 @@ import { AnimalCategoryComponent } from './newProject9/Rania/animal-category/ani
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'Register', component: RegesterComponent },
+      { path: 'Login', component: LoginComponent },
       { path: 'Shelters', component: SheltersComponent },
       { path: 'Animals', component: AnimalsComponent },
       { path: 'Shelters', component: SheltersComponent },
       { path: 'AboutUs', component: AboutUSComponent },
       { path: 'Shelters', component: SheltersComponent },
-      { path: 'post-communityuser', component:PostCommunityuserComponent  },
+      { path: 'post-communityuser', component: PostCommunityuserComponent },
+
 
 
     ])
